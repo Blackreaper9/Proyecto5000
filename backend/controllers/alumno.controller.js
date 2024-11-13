ee6f063782d58b570dc35752d3cb5edd572d5863
Blackreaper9 <utm23090485@utma.edu.mx>
2024-11-13 16:35:27 -0600

    Proyecto5000

---
+++ backend/controllers/alumno.controller.js
@@ -0,0 +1,9 @@
+import { modelo1 } from "../models/alumno.models.js";
+
+modelo1.create({
+    name: "chuyin pinguin"
+})
+
+export const test = ()=>{
+    console.log("si funciona el controlador")
+}
